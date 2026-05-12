a8a52934b3e41f0648247f5648afd3b1f9e8482b
duskynumber5 <mplobo@ucsc.edu>
2026-05-12 16:39:08 -0700

    animations done!

--- CMPM171/Assets/Scripts/Player.cs
+++ CMPM171/Assets/Scripts/Player.cs
@@ -8,6 +8,9 @@ public class Player : MonoBehaviour
     public float groundCheckRadius = 0.2f;
     public LayerMask groundLayer;
 
+    public Vector2 respawnPoint;
+    private SpriteRenderer spriteRenderer;
+    public Animator animator;
     // public PuzzleUIManager puzzleUIManager;
 
     private Rigidbody2D rb;
@@ -18,22 +21,50 @@ public class Player : MonoBehaviour
     {
         rb = GetComponent<Rigidbody2D>();
         rb.freezeRotation = true;
+
+        respawnPoint = rb.position;
+
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     void Update()
     {
         float moveInput = Input.GetAxis("Horizontal");
         rb.linearVelocity = new Vector2(moveInput * moveSpeed, rb.linearVelocity.y);
+
+        animator.SetFloat("Speed", Mathf.Abs(moveInput));
     
         if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
         {
             rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
         }
+
+        if (!isGrounded)
+        {
+            animator.SetBool("isJumping", true);
+        }
+        else
+        {
+            animator.SetBool("isJumping", false);
+        }
+
+        if (transform.position.y < -10f)
+        {
+            Respawn();
+        }
+
+        spriteRenderer.flipX = rb.linearVelocity.x < 0;
     }
 
     private void FixedUpdate()
     {
         isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
+    }
+
+    private void Respawn()
+    {
+        rb.position = respawnPoint;
+        rb.linearVelocity = Vector2.zero;
     }
 
 }
